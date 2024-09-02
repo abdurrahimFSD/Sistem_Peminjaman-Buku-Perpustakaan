97f8e0fd175ae4c4daa135801e21a067fc9e45c4
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-02 17:24:09 +0800

    Menambahkan percabangan switch case untuk routing halaman berdasarkan parameter page

--- index.php
+++ index.php
@@ -2,5 +2,15 @@
 include('./layouts/mainWrapper.php');
 include('./layouts/sidebar.php');
 include('./layouts/pageWrapper.php');
+
+switch($_GET['page']) {
+    case 'home':
+        include('./home.php');
+        break;
+
+    default:
+        include('./home.php');
+}
+
 include('./layouts/footer.php');
 ?>
