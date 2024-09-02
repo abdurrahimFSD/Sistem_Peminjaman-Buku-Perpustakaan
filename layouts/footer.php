b43f7c510d405e13877f4e5329b119d16e2a1268
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-02 17:18:06 +0800

    Menambahkan kode untuk layout footer

---
+++ layouts/footer.php
@@ -0,0 +1,58 @@
+        </div>
+        <!-- Page Wrapper End -->
+
+        <!--  Search Bar -->
+        <div class="modal fade mt-9" id="searchingModal" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable modal-lg">
+                <div class="modal-content rounded-1">
+                    <div class="modal-header border-bottom">
+                        <input type="search" class="form-control fs-3" placeholder="Search here" id="search" />
+                        <a href="javascript:void(0)" data-bs-dismiss="modal" class="lh-1">
+                            <iconify-icon icon="tabler:x" class="fs-5 ms-3 align-middle"></iconify-icon>
+                        </a>
+                    </div>
+                    <div class="modal-body message-body" data-simplebar="">
+                        <h5 class="mb-0 fs-5 p-1">Quick Page Links</h5>
+                        <ul class="list mb-0 py-2">
+                            <li class="p-1 mb-1 bg-hover-light-black">
+                                <a href="javascript:void(0)">
+                                    <span class="d-block">Dashboard</span>
+                                    <span class="text-muted d-block">/home/dashboard</span>
+                                </a>
+                            </li>
+                            <li class="p-1 mb-1 bg-hover-light-black">
+                                <a href="javascript:void(0)">
+                                    <span class="d-block">Dashboard2</span>
+                                    <span class="text-muted d-block">/home/dashboard2</span>
+                                </a>
+                            </li>
+                            <li class="p-1 mb-1 bg-hover-light-black">
+                                <a href="javascript:void(0)">
+                                    <span class="d-block">Menu</span>
+                                    <span class="text-muted d-block">/apps/menu</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- Main Wrapper End -->
+    
+    <div class="dark-transparent sidebartoggler"></div>
+
+    <!-- iconify icons -->
+    <script src="./assets/libs/iconify-icon/iconify-icon.min.js"></script>
+    
+    <script src="./assets/js/vendor.min.js"></script>
+    <!-- Import Js Files -->
+    <script src="./assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/libs/simplebar/dist/simplebar.min.js"></script>
+    <script src="./assets/js/theme/app.init.js"></script>
+    <script src="./assets/js/theme/theme.js"></script>
+    <script src="./assets/js/theme/app.min.js"></script>
+    <script src="./assets/js/theme/sidebarmenu.js"></script>
+</body>
+</html>
