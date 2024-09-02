01ea512d87a3a03e53f10940f55c2393510ce745
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-02 21:01:30 +0800

    Memperbarui link dengan parameter bukuData agar mengarah ke halaman Data Buku

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -57,7 +57,7 @@
                             </a>
                             <ul aria-expanded="false" class="collapse first-level">
                                 <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">
+                                    <a href="?page=bukuData" class="sidebar-link">
                                         <div class="round-16 d-flex align-items-center justify-content-center">
                                             <iconify-icon icon="tabler:circle"></iconify-icon>
                                         </div>
