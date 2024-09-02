211652f48b9e8677fed382985c703bc4aed5d9e4
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-02 17:16:40 +0800

    Menambahkan kode untuk layout sidebar

---
+++ layouts/sidebar.php
@@ -0,0 +1,177 @@
+        <!-- Sidebar Start -->
+        <aside class="left-sidebar with-vertical">
+            <div>
+                <!-- ---------------------------------- -->
+                <!-- Start Vertical Layout Sidebar -->
+                <!-- ---------------------------------- -->
+
+                <!-- Start Logo -->
+                <div class="brand-logo d-flex align-items-center justify-content-between">
+                    <a href="#" class="text-nowrap logo-img">
+                        <h1 class="mb-0">
+                            <iconify-icon icon="tdesign:system-code" class="align-middle ms-1 me-1"></iconify-icon> <span class="align-middle">APP</span>
+                        </h1>
+                    </a>
+                    <a href="javascript:void(0)" class="sidebartoggler ms-auto mt-2 text-decoration-none fs-5 d-block d-xl-none">
+                        <iconify-icon icon="tabler:x"></iconify-icon>
+                    </a>
+                </div>
+                <!-- End Logo -->
+                
+                <!-- Start Sidebar Items -->
+                <nav class="sidebar-nav scroll-sidebar" data-simplebar>
+                    <ul id="sidebarnav">
+                        <!-- ---------------------------------- -->
+                        <!-- Start Home -->
+                        <!-- ---------------------------------- -->
+                        <li class="nav-small-cap">
+                            <iconify-icon icon="tabler:dots" class="nav-small-cap-icon fs-4"></iconify-icon>
+                            <!-- <span class="nav-small-cap-icon fs-1">Home</span> -->
+                            <span class="hide-menu">Home</span>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="" id="get-url" aria-expanded="false">
+                                <span>
+                                    <iconify-icon icon="tabler:device-analytics"></iconify-icon>
+                                </span>
+                                <span class="hide-menu">Dashboard</span>
+                            </a>
+                        </li>
+                        <!-- ---------------------------------- -->
+                        <!-- End Home -->
+                        <!-- ---------------------------------- -->
+                        
+                        <!-- ---------------------------------- -->
+                        <!-- Start Apps -->
+                        <!-- ---------------------------------- -->
+                        <li class="nav-small-cap">
+                            <iconify-icon icon="tabler:dots" class="nav-small-cap-icon fs-4"></iconify-icon>
+                            <span class="hide-menu">Apps</span>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="#" aria-expanded="false">
+                                <span>
+                                    <iconify-icon icon="tabler:link"></iconify-icon>
+                                </span>
+                                <span class="hide-menu">Menu</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
+                                <span class="d-flex">
+                                    <iconify-icon icon="tabler:link-plus"></iconify-icon>
+                                </span>
+                                <span class="hide-menu">Sub Menu</span>
+                            </a>
+                            <ul aria-expanded="false" class="collapse first-level">
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link">
+                                        <div class="round-16 d-flex align-items-center justify-content-center">
+                                            <iconify-icon icon="tabler:circle"></iconify-icon>
+                                        </div>
+                                        <span class="hide-menu">Link</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link">
+                                        <div class="round-16 d-flex align-items-center justify-content-center">
+                                            <iconify-icon icon="tabler:circle"></iconify-icon>
+                                        </div>
+                                        <span class="hide-menu">Link</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- ---------------------------------- -->
+                        <!-- End Apps -->
+                        <!-- ---------------------------------- -->
+                        
+                        
+                        
+                        <!-- ---------------------------------- -->
+                        <!-- Start OTHER -->
+                        <!-- ---------------------------------- -->
+                        <li class="nav-small-cap">
+                            <iconify-icon icon="tabler:dots" class="nav-small-cap-icon fs-4"></iconify-icon>
+                            <span class="hide-menu">OTHER</span>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="#" aria-expanded="false">
+                                <span>
+                                    <iconify-icon icon="tabler:link"></iconify-icon>
+                                </span>
+                                <span class="hide-menu">Menu</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
+                                <span class="d-flex">
+                                    <iconify-icon icon="tabler:box-multiple"></iconify-icon>
+                                </span>
+                                <span class="hide-menu">Menu Level</span>
+                            </a>
+                            <ul aria-expanded="false" class="collapse first-level">
+                                <li class="sidebar-item">
+                                    <a href="javascript:void(0)" class="sidebar-link">
+                                        <div class="round-16 d-flex align-items-center justify-content-center">
+                                            <iconify-icon icon="tabler:circle"></iconify-icon>
+                                        </div>
+                                        <span class="hide-menu">Level 1</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
+                                        <div class="round-16 d-flex align-items-center justify-content-center">
+                                            <iconify-icon icon="tabler:circle"></iconify-icon>
+                                        </div>
+                                        <span class="hide-menu">Level 1.1</span>
+                                    </a>
+                                    <ul aria-expanded="false" class="collapse two-level">
+                                        <li class="sidebar-item">
+                                            <a href="javascript:void(0)" class="sidebar-link">
+                                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                                    <iconify-icon icon="tabler:circle"></iconify-icon>
+                                                </div>
+                                                <span class="hide-menu">Level 2</span>
+                                            </a>
+                                        </li>
+                                        <li class="sidebar-item">
+                                            <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
+                                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                                    <iconify-icon icon="tabler:circle"></iconify-icon>
+                                                </div>
+                                                <span class="hide-menu">Level 2.1</span>
+                                            </a>
+                                            <ul aria-expanded="false" class="collapse three-level">
+                                                <li class="sidebar-item">
+                                                    <a href="javascript:void(0)" class="sidebar-link">
+                                                        <div class="round-16 d-flex align-items-center justify-content-center">
+                                                            <iconify-icon icon="tabler:circle"></iconify-icon>
+                                                        </div>
+                                                        <span class="hide-menu">Level 3</span>
+                                                    </a>
+                                                </li>
+                                                <li class="sidebar-item">
+                                                    <a href="javascript:void(0)" class="sidebar-link">
+                                                        <div class="round-16 d-flex align-items-center justify-content-center">
+                                                            <iconify-icon icon="tabler:circle"></iconify-icon>
+                                                        </div>
+                                                        <span class="hide-menu">Level 3.1</span>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- End Sidebari Items -->
+
+                <!-- ---------------------------------- -->
+                <!-- End Vertical Layout Sidebar -->
+                <!-- ---------------------------------- -->
+            </div>
+        </aside>
+        <!--  Sidebar End -->
