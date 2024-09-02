8db81ea3d4ce4a2abc8601b9b4bc589c48861d13
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-02 20:50:33 +0800

    Mengubah label 'link' menjadi 'Tambah Data' di submenu 'Buku'

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -61,7 +61,7 @@
                                         <div class="round-16 d-flex align-items-center justify-content-center">
                                             <iconify-icon icon="tabler:circle"></iconify-icon>
                                         </div>
-                                        <span class="hide-menu">Link</span>
+                                        <span class="hide-menu">Tambah Data</span>
                                     </a>
                                 </li>
                                 <li class="sidebar-item">
