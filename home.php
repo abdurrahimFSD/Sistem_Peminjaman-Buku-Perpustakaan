e6c44776a633c5984bdf87f80d26f1af22fc0a33
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-02 17:24:49 +0800

    Menambahkan konten sementara ke halaman home.php

---
+++ home.php
@@ -0,0 +1,9 @@
+<!-- Body Wrapper Start -->
+<div class="body-wrapper ">
+    <div class="container-fluid">
+        
+        <h1>Halaman Dashboard</h1>
+
+    </div>
+</div>
+<!-- Body Wrapper End -->
