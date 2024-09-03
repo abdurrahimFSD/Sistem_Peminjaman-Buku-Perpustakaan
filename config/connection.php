179de4c205d966223871ebc8bc0b84a559ac32da
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-03 10:18:32 +0800

    Menambahkan variable connection untuk menghubungkan ke database menggunakan mysqli_connect

--- config/connection.php
+++ config/connection.php
@@ -3,4 +3,6 @@ $serverName = "localhost";
 $username = "root";
 $password = "";
 $databaseName = "sistem_peminjaman-buku-perpustakaan";
+
+$connection = mysqli_connect($serverName, $username, $password, $databaseName);
 ?>
