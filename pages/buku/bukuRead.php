291112bd81d8b16539c46c72910b1987738fb6bd
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-03 11:54:24 +0800

    Menambahkan template kode card untuk menampilkan data buku

--- pages/buku/bukuRead.php
+++ pages/buku/bukuRead.php
@@ -27,6 +27,81 @@
             </div>
             <!-- End Breadcrumb -->
 
+            <!-- Start Main Content -->
+            <div class="widget-content searchable-container list">
+
+                <!-- Start card body content -->
+                <div class="card card-body">
+                    <div class="table-responsive">
+                        <table class="table search-table align-middle text-nowrap">
+                            <thead class="header-item">
+                                <th>
+                                    <div class="n-chk align-self-center text-center">
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input primary" id="contact-check-all" />
+                                            <label class="form-check-label" for="contact-check-all"></label>
+                                            <span class="new-control-indicator"></span>
+                                        </div>
+                                    </div>
+                                </th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Location</th>
+                                <th>Phone</th>
+                                <th>Action</th>
+                            </thead>
+                            <tbody>
+                                <!-- start row -->
+                                <tr class="search-items">
+                                    <td>
+                                        <div class="n-chk align-self-center text-center">
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input contact-chkbox primary" id="checkbox1" />
+                                                <label class="form-check-label" for="checkbox1"></label>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <img src="./assets/images/profile/user-2.jpg" alt="avatar" class="rounded-circle" width="35" />
+                                            <div class="ms-3">
+                                                <div class="user-meta-info">
+                                                    <h6 class="user-name mb-0" data-name="Emma Adams">Emma Adams</h6>
+                                                    <span class="user-work fs-3" data-occupation="Web Developer">Web Developer</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <span class="usr-email-addr" data-email="adams@mail.com">adams@mail.com</span>
+                                    </td>
+                                    <td>
+                                        <span class="usr-location" data-location="Boston, USA">Boston, USA</span>
+                                    </td>
+                                    <td>
+                                        <span class="usr-ph-no" data-phone="+1 (070) 123-4567">+91 (070) 123-4567</span>
+                                    </td>
+                                    <td>
+                                        <div class="action-btn">
+                                            <a href="javascript:void(0)" class="text-primary edit">
+                                                <iconify-icon icon="tabler:eye" class="fs-5"></iconify-icon>
+                                            </a>
+                                            <a href="javascript:void(0)" class="text-dark delete ms-2">
+                                                <iconify-icon icon="tabler:trash" class="fs-5"></iconify-icon>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <!-- end row -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- End Card Body Content -->
+
+            </div>
+            <!-- End Main Content -->
+
     </div>
 </div>
 <!-- Body Wrapper End -->
