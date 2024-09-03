0f7bb32026f046045534b090a32212d3106910d8
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-03 09:36:32 +0800

    Memperbarui link dengan parameter 'peminjamanData' agar mengarah ke halaman Data Peminjaman

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -138,7 +138,7 @@
                             </a>
                             <ul aria-expanded="false" class="collapse first-level">
                                 <li class="sidebar-item">
-                                    <a href="?page=anggotaData" class="sidebar-link">
+                                    <a href="?page=peminjamanData" class="sidebar-link">
                                         <div class="round-16 d-flex align-items-center justify-content-center">
                                             <iconify-icon icon="tabler:circle"></iconify-icon>
                                         </div>
