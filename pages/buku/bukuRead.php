bd83b8b173607821bfe879a0e721989d1546797b
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-03 18:05:05 +0800

    Memperbarui alt

--- pages/buku/bukuRead.php
+++ pages/buku/bukuRead.php
@@ -53,7 +53,7 @@
                                     </td>
                                     <td>
                                         <div class="d-flex align-items-center">
-                                            <img src="./assets/images/book/JavaScript The Good Parts.jpg" alt="avatar" class="rounded" width="50" />
+                                            <img src="./assets/images/book/JavaScript The Good Parts.jpg" alt="Book" class="rounded" width="50" />
                                             <div class="ms-3">
                                                 <div>
                                                     <h6 class="mb-0">JavaScript Programming</h6>
