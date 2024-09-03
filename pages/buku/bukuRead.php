bc6c5c9fe65883ab374c638806fb6b30e595ab24
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-03 11:06:23 +0800

    Memperbarui breadcrumb menjadi 'Data Buku' di mejaRead.php

--- pages/buku/bukuRead.php
+++ pages/buku/bukuRead.php
@@ -7,7 +7,7 @@
                 <div class="card-body px-4 py-3">
                     <div class="row align-items-center">
                         <div class="col-9">
-                            <h4 class="fw-semibold mb-8">Data</h4>
+                            <h4 class="fw-semibold mb-8">Data Buku</h4>
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item">
