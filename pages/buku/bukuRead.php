becafa16bd091349ce494b372e5915af94aa5d79
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-03 21:36:10 +0800

    Menambahkan aria-label

--- pages/buku/bukuRead.php
+++ pages/buku/bukuRead.php
@@ -73,10 +73,10 @@
                                             <a href="javascript:void(0)" class="btn btn-outline-info btn-sm d-inline-flex" data-bs-toggle="tooltip" data-bs-title="Detail" aria-label="Detail">
                                                 <iconify-icon icon="tabler:info-square" class="fs-5"></iconify-icon>
                                             </a>
-                                            <a href="javascript:void(0)" class="btn btn-outline-warning btn-sm d-inline-flex mx-1" data-bs-toggle="tooltip" data-bs-title="Edit">
+                                            <a href="javascript:void(0)" class="btn btn-outline-warning btn-sm d-inline-flex mx-1" data-bs-toggle="tooltip" data-bs-title="Edit" aria-label="Edit">
                                                 <iconify-icon icon="tabler:pencil" class="fs-5"></iconify-icon>
                                             </a>
-                                            <a href="javascript:void(0)" class="btn btn-outline-danger btn-sm d-inline-flex delete" data-bs-toggle="tooltip" data-bs-title="Hapus">
+                                            <a href="javascript:void(0)" class="btn btn-outline-danger btn-sm d-inline-flex delete" data-bs-toggle="tooltip" data-bs-title="Hapus" aria-label="Hapus">
                                                 <iconify-icon icon="tabler:trash" class="fs-5"></iconify-icon>
                                             </a>
                                         </div>
