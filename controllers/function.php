d0fd1b95a2dd0b5e279bbbb8043500754afda944
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-04 09:24:13 +0800

    Menambahkan kode untuk include connection

---
+++ controllers/function.php
@@ -0,0 +1,7 @@
+<?php
+// Memeriksa apakah file 'connection.php' ada di dalam folder config
+if (file_exists('./config/connection.php')) {
+    // Jika ada maka include connection.php
+    include('./config/connection.php');
+}
+?>
