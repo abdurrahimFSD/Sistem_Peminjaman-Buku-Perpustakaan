b7816650adcb1fab5eab76f64349dc4a46511f56
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-04 09:25:49 +0800

    Menambahkan include connection

--- controllers/function.php
+++ controllers/function.php
@@ -3,5 +3,7 @@
 if (file_exists('./config/connection.php')) {
     // Jika ada maka include connection.php
     include('./config/connection.php');
+} elseif (file_exists('../config/connection.php')) {
+    include('../config/connection.php');
 }
 ?>
