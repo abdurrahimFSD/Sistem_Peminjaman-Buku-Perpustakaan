9d3dc5dd389092e7b9b28af8979dcd6b08f314a7
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-04 16:48:10 +0800

    Menambahkan input Tahun Terbit

--- pages/buku/bukuCreate.php
+++ pages/buku/bukuCreate.php
@@ -43,6 +43,10 @@
                             <label for="isbn" class="form-label">ISBN</label>
                             <input type="text" name="isbn" id="isbn" class="form-control" placeholder="978-11-0001" required>
                         </div>
+                        <div class="mb-3">
+                            <label for="tahunTerbit" class="form-label">Tahun Terbit</label>
+                            <input type="date" name="tahunTerbit" id="tahunTerbit" class="form-control" required>
+                        </div>
                     </form>
                 </div>
                 <!-- ENd Card body main content -->
