dd652e74e32b9d11010a0839e95a9e7f61c03243
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-05 15:55:47 +0700

    Menambahkan kode untuk function uploadFile

--- controllers/function.php
+++ controllers/function.php
@@ -33,7 +33,38 @@ function fetchData($tableName) {
 
 // Function uploadFile
 function uploadFile($file) {
+    $namaFile = $file['foto']['name'];
+    $ukuranFile = $file['foto']['size'];
+    $error = $file['foto']['error'];
+    $tmpName = $file['foto']['tmp_name'];
 
+    // Cek apakah ada file yg diupload
+    if ($error === 4) {
+        return false;
+    }
+
+    // Cek apakah file yg diupload adalah gambar
+    $ektensiFileValid = ['jpg', 'jpeg', 'png', 'svg'];
+    $ektensiFile = explode('.', $namaFile);
+    $ektensiFile = strtolower(end($ektensiFile));
+    if (!in_array($ektensiFile, $ektensiFileValid)) {
+        return false;
+    }
+
+    // Cek ukuran file jika terlalu besar (misal, lebih dari 2MB)
+    if ($ukuranFile > 2000000) {
+        return false;
+    }
+
+    // Generate nama file baru yg unik
+    $namaFileBaru = uniqid();
+    $namaFileBaru .= '.';
+    $namaFileBaru .= $ektensiFile;
+
+    // Pindahkan ke folder uploads/images/buku
+    move_uploaded_file($tmpName, './uploads/images/buku/' . $namaFileBaru);
+
+    return $namaFileBaru;
 }
 
 // Function bukuCreate untuk menambahkan data buku baru ke database
