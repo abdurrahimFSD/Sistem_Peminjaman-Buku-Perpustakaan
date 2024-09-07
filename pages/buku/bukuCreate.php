9ce4f8a95d9110540d482955bbdfc09aa5b05b90
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-07 20:10:40 +0800

    Menambahkan elemen div untuk menampilkan pesan error pada input file

--- pages/buku/bukuCreate.php
+++ pages/buku/bukuCreate.php
@@ -71,6 +71,7 @@ $kategori = ['Teknologi', 'Ilmu Pengetahuan', 'Pendidikan', 'Agama', 'Kesehatan'
                             <label for="foto" class="form-label">Foto</label>
                             <input type="file" name="foto" id="foto" class="form-control" onchange="previewImage(event)">
                             <img id="imgPreview" src="" alt="Pratinjau Foto" class="mt-3" style="max-width: 100px; display: none;">
+                            <div id="fileError" class="text-danger mt-2" style="display: none;"></div>
                         </div>
                         <a href="?page=bukuData" class="d-inline-flex justify-content-center align-items-center btn btn-outline-secondary me-2">
                             <iconify-icon icon="fluent:arrow-left-24-filled" class="me-1 fs-5"></iconify-icon>Kembali
