109b92c18a8e42a406462f156648b31742ece8aa
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-07 20:24:01 +0800

    Memperbarui validasi dengan menambahkan pengecekan ektensi dan ukuran file pada preview gambar di form

--- pages/buku/bukuCreate.php
+++ pages/buku/bukuCreate.php
@@ -92,6 +92,9 @@ $kategori = ['Teknologi', 'Ilmu Pengetahuan', 'Pendidikan', 'Agama', 'Kesehatan'
 <script>
 function previewImage(event) {
     const file = event.target.files[0];
+    const fileError = document.getElementById('fileError'); // Mengambil elemen error
+    fileError.style.display = 'none'; // Menyembunyikan pesan error saat file baru dipilih
+
     if (file) {
         const reader = new FileReader();
         reader.onload = function(e) {
@@ -100,6 +103,25 @@ function previewImage(event) {
             imgPreview.style.display = 'block'; // Menampilkan elemen img setelah file diunggah
         }
         reader.readAsDataURL(file); // Membaca file sebagai URL data
+
+        // Cek apakah file valid
+        const validExtensions = ['jpg', 'jpeg', 'png', 'svg'];
+        const fileExtension = file.name.split('.').pop().toLowerCase();
+
+        if (!validExtensions.includes(fileExtension)) {
+            fileError.textContent = 'File yang diupload harus berupa gambar (jpg, jpeg, png, svg)';
+            fileError.style.display = 'block'; // Menampilkan pesan error
+            document.getElementById('imgPreview').style.display = 'none'; // Menyeembunyikan preview
+            return;
+        }
+
+        // Cek ukuran file
+        if (file.size > 2000000) { // maksimal 2MB
+            fileError.textContent = 'Ukuran file terlalu besar! Maksimal 2MB';
+            fileError.style.display = 'block'; // Menampilkan pesan error
+            document.getElementById('imgPreview').style.display = 'none'; // Menyembunyikan preview
+            return;
+        }
     }
 }
 </script>
