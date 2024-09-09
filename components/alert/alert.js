5633815f0af83389584793e4bac32974398d1385
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-09 10:11:33 +0800

    Menambahkan alert untuk file tidak valid di alert.js

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -24,4 +24,14 @@ if (status === 'successBukuCreate') {
             history.back();
         }
     });
+} else if (status === 'fileTidakValid') {
+    Swal.fire({
+        title: 'Gagal',
+        text: 'File yang diupload harus berupa gambar (jpg, jpeg, png, svg)',
+        icon: 'warning'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            history.back();
+        }
+    });
 }
