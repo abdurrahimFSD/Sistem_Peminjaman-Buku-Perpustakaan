29c64cbbcdb57146c519dd0e48b289312b8a983d
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-09 09:45:01 +0800

    Menambahkan redirect ke halaman bukuCreate dengan status success setelah berhasil menambahkan buku baru

--- controllers/process.php
+++ controllers/process.php
@@ -7,6 +7,11 @@ if (isset($_POST['simpan'])) {
     if ($_POST['simpan'] == 'bukuCreate') {
         // Memanggil function bukuCreate
         $result = bukuCreate($_POST, $_FILES);
+
+        // Jika result mengembalikan success
+        if ($result == 'success') {
+            header("Location: ../index.php?page=bukuCreate&status=successBukuCreate");
+        }
     }
 }
 ?>
