ecb547a8a44f99ecb154b1033f255e914c087e60
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-09 09:47:13 +0800

    Menambahkan penanganan redirect saat terjadi duplicate isbn

--- controllers/process.php
+++ controllers/process.php
@@ -11,6 +11,10 @@ if (isset($_POST['simpan'])) {
         // Jika result mengembalikan success
         if ($result == 'success') {
             header("Location: ../index.php?page=bukuCreate&status=successBukuCreate");
+        } elseif (strpos($result, 'duplicateIsbn') !== false) {
+            // Ambil isbn yg duplicate
+            $isbnDuplicate = explode(':', $result)[1];
+            header("Location: ../index.php?page=bukuCreate&status=duplicateIsbn&isbn=$isbnDuplicate");
         }
     }
 }
