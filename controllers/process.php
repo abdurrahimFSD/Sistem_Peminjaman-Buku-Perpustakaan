4490d73ad25a79ae09bc8d80cb8e42798625bc0a
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-09 09:42:22 +0800

    Menambahkan proses pengecekan form submit dan pemanggilan function bukuCreate

--- controllers/process.php
+++ controllers/process.php
@@ -1,3 +1,12 @@
 <?php
 include('./function.php');
+
+// Mengecek apakah form telah disubmit atau disimpan
+if (isset($_POST['simpan'])) {
+    // Jika tombol simpan adalah bukuCreate
+    if ($_POST['simpan'] == 'bukuCreate') {
+        // Memanggil function bukuCreate
+        $result = bukuCreate($_POST, $_FILES);
+    }
+}
 ?>
