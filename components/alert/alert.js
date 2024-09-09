7f01d92fe0b058ddd2fafcceb029b9834b155b54
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-09 10:08:48 +0800

    Menambahkan alert sukses untuk operasi penambahan buku di alert.js

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -2,3 +2,16 @@
 const urlParams = new URLSearchParams(window.location.search);
 const status = urlParams.get('status');
 const kodeIsbn = urlParams.get('isbn');
+
+// Kode alert untuk operasi create atau tambah
+if (status === 'successBukuCreate') {
+    Swal.fire({
+        title: 'Berhasil',
+        text: 'Data buku berhasil ditambahkan',
+        icon: 'success'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = './index.php?page=bukuData';
+        }
+    });
+} 
