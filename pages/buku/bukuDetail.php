1a859bd748848a80c471ca73bf47587d6fa01f9b
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-14 11:16:54 +0700

    Menambahkan label ID Buku' pada kolom detail buku

--- pages/buku/bukuDetail.php
+++ pages/buku/bukuDetail.php
@@ -36,10 +36,10 @@
                     <tbody>
                         <tr>
                             <td class="col-6 col-md-3">
-
+                                <p class="mb-0 text-dark fw-bolder">ID Buku</p>
                             </td>
                             <td class="col-6 col md-9">
-                                
+
                             </td>
                         </tr>
                     </tbody>
