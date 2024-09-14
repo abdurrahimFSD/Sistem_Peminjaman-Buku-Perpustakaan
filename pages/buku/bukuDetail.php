710a7130af9e8a70ce6d3385fc2a31f26d96ac90
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-14 11:51:20 +0700

    Menambakan element kosong yg nantinya menampilkan data kategori dengan dinamis

--- pages/buku/bukuDetail.php
+++ pages/buku/bukuDetail.php
@@ -79,7 +79,7 @@
                                 <p class="mb-0 text-dark fw-bolder">Kategori</p>
                             </td>
                             <td class="col-6 col-md-9">
-
+                                <p class="mb-0 text-dark"></p>
                             </td>
                         </tr>
                     </tbody>
