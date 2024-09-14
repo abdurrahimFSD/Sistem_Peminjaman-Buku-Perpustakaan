68a2bd14f7ca3c8b345beb80ebcb23cd77b6c7fc
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-14 20:40:56 +0700

    Menambahkan input Tahun terbit

--- pages/buku/bukuUpdate.php
+++ pages/buku/bukuUpdate.php
@@ -42,6 +42,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="tahunTerbit" class="form-label">Tahun Terbit</label>
+                        <input type="date" name="tahunTerbit" id="tahunTerbit" class="form-control" onfocus="this.showPicker()" required>
                     </div>
                 </form>
             </div>
