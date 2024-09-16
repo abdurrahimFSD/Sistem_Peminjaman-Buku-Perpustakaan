034808f54eab1043ce7430b6533f3693daeae4fd
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-16 16:51:14 +0700

    Menambahkan card body

--- pages/anggota/anggotaRead.php
+++ pages/anggota/anggotaRead.php
@@ -27,6 +27,12 @@
         </div>
         <!-- End Breadcrumb -->
     
+        <!-- Start card body content -->
+        <div class="card card-body">
+            
+        </div>
+        <!-- End Card Body Content -->
+        
     </div>
 </div>
 <!-- Body Wrapper End -->
