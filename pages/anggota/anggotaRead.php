be355d5fdae71cc6bd7a13e4d1f7944594e71fb6
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-16 17:27:42 +0700

    Menambahkan element a

--- pages/anggota/anggotaRead.php
+++ pages/anggota/anggotaRead.php
@@ -56,6 +56,7 @@
                                 <a href="" class="btn btn-outline-warning btn-sm d-inline-flex me-1" data-bs-toggle="tooltip" data-bs-title="Edit" aria-label="Edit">
                                     <iconify-icon icon="tabler:pencil" class="fs-5"></iconify-icon>
                                 </a>
+                                <a href=""></a>
                             </td>
                         </tr>
                     </tbody>
