597e64141d576357d487190691884bcd7057635a
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-16 17:30:20 +0700

    Menambahkan icon trash untuk button aksi hapus

--- pages/anggota/anggotaRead.php
+++ pages/anggota/anggotaRead.php
@@ -56,7 +56,9 @@
                                 <a href="" class="btn btn-outline-warning btn-sm d-inline-flex me-1" data-bs-toggle="tooltip" data-bs-title="Edit" aria-label="Edit">
                                     <iconify-icon icon="tabler:pencil" class="fs-5"></iconify-icon>
                                 </a>
-                                <a href="" class="btn btn-outline-danger btn-sm d-inline-flex" data-bs-toggle="tooltip" data-bs-title="Hapus" aria-label="Hapus"></a>
+                                <a href="" class="btn btn-outline-danger btn-sm d-inline-flex" data-bs-toggle="tooltip" data-bs-title="Hapus" aria-label="Hapus">
+                                    <iconify-icon icon="tabler:trash" class="fs-5"></iconify-icon>
+                                </a>
                             </td>
                         </tr>
                     </tbody>
