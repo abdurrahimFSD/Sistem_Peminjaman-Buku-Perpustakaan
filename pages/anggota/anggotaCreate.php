77a4e232236f971e69672ce0840a524faa801554
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-16 20:32:30 +0700

    Menambahkan card-title dengan text Masukkan Data Anggota

--- pages/anggota/anggotaCreate.php
+++ pages/anggota/anggotaCreate.php
@@ -29,7 +29,7 @@
 
         <!-- Start Card Body -->
         <div class="card card-body">
-            
+            <h4 class="card-title">Masukkan Data Anggota</h4>
         </div>
         <!-- End Card Body -->
     
