39cfe3e2bf587f7977a579ba031cc4d35bf57f78
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-17 11:23:55 +0700

    Mengirim data form menggunakan Fetch API setelah konfirmasi simpan

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -69,6 +69,11 @@ if (document.getElementById('simpanAnggotaUpdate')) {
             if (result.isConfirmed) {
                 const form = document.getElementById('anggotaUpdateForm');
                 const formData = new FormData(form);
+
+                fetch('./controllers/process.php', {
+                    method: 'POST',
+                    body: formData
+                })
             }
         })
     })
