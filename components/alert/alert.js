5e76a86fddccc8bd4629e6fc730152b679a7a9f5
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-18 09:10:28 +0700

    Menambahkan logika pengecekan result.isConfirmed untuk menjalankan aksi setelah konfirmasi hapus data

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -107,7 +107,9 @@ if (document.getElementById('deleteButtonAnggota')) {
             confirmButtonText: "Ya, hapus",
             cancelButtonText: 'Batal'
         }).then((result) => {
-            
+            if (result.isConfirmed) {
+                
+            }
         })
     }
 }
