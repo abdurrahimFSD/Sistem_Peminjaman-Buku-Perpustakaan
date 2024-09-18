6078d7040361e69d5aadb782dd62aadcec718816
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-18 09:03:33 +0700

    Menambahkan function confirmDelete(id) untuk menangani penghapusan data anggota

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -96,4 +96,7 @@ if (document.getElementById('simpanAnggotaUpdate')) {
 
 // Kode alert untuk operasi delete atau hapus
 if (document.getElementById('deleteButtonAnggota')) {
+    function confirmDelete(id) {
+
+    }
 }
