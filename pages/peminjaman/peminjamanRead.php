5d4173f3cf4e8b698b6892fcd146041b7532435f
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-18 10:37:56 +0700

    Menambahkan button untuk hapus data

--- pages/peminjaman/peminjamanRead.php
+++ pages/peminjaman/peminjamanRead.php
@@ -56,6 +56,9 @@
                             <a href="" class="btn btn-outline-warning btn-sm d-inline-flex mx-1" data-bs-toggle="tooltip" data-bs-title="Edit" aria-label="Edit">
                                 <iconify-icon icon="tabler:pencil" class="fs-5"></iconify-icon>
                             </a>
+                            <a href="" class="btn btn-outline-danger btn-sm d-inline-flex delete" data-bs-toggle="tooltip" data-bs-title="Hapus" aria-label="Hapus">
+                                <iconify-icon icon="tabler:trash" class="fs-5"></iconify-icon>
+                            </a>
                         </td>
                     </tbody>
                 </table>
