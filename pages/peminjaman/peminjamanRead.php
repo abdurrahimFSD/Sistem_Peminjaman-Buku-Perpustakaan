5f458408ad1834394210aa6d037575090ebafe6d
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-18 10:36:35 +0700

    Menambahkan button untuk Edit Data

--- pages/peminjaman/peminjamanRead.php
+++ pages/peminjaman/peminjamanRead.php
@@ -53,6 +53,9 @@
                             <a href="" class="btn btn-outline-info btn-sm d-inline-flex" data-bs-toggle="tooltip" data-bs-title="Detail" aria-label="Detail">
                                 <iconify-icon icon="tabler:info-square" class="fs-5"></iconify-icon>
                             </a>
+                            <a href="" class="btn btn-outline-warning btn-sm d-inline-flex mx-1" data-bs-toggle="tooltip" data-bs-title="Edit" aria-label="Edit">
+                                <iconify-icon icon="tabler:pencil" class="fs-5"></iconify-icon>
+                            </a>
                         </td>
                     </tbody>
                 </table>
