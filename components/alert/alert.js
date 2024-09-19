764bd5c9a91b74d89bc49d4f85fd82d36d84b442
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-19 11:23:04 +0700

    Menambahkan blok penanganan response untuk memproses hasil dari fetch

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -76,6 +76,9 @@ if (document.getElementById('peminjamanCreateForm')) {
                     body: formData
                 })
                 .then(response => response.text())
+                .then(response => {
+                    
+                })
             }
         })
     }) 
