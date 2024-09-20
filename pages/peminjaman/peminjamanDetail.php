0b62fcd7570b7613fe2015014df1b3afc0b01845
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-20 09:14:50 +0700

    Menambahkan informasi ID Pinjam pada tabel detail peminjaman

--- pages/peminjaman/peminjamanDetail.php
+++ pages/peminjaman/peminjamanDetail.php
@@ -57,7 +57,7 @@ if ($resultGetDetail) {
                     <tbody>
                         <tr>
                             <td class="col-6 col-md-3">
-                                
+                                <p class="mb-0 text-dark fw-bolder">ID Pinjam</p>
                             </td>
                         </tr>
                     </tbody>
