ab5ca7d3b3e60ee7780fc0b12e94be2e86f86300
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-20 09:26:54 +0700

    Menambahkan tr

--- pages/peminjaman/peminjamanDetail.php
+++ pages/peminjaman/peminjamanDetail.php
@@ -79,6 +79,9 @@ if ($resultGetDetail) {
                                 <p class="mb-0 text-dark"><?= htmlspecialchars($peminjamanData['tanggal_kembali']); ?></p>
                             </td>
                         </tr>
+                        <tr>
+                            
+                        </tr>
                     </tbody>
                 </table>
             </div>
