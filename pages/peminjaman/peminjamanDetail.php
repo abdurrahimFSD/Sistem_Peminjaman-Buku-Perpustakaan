4ea1429b3ab2d3173ddf4952fc2552493c65fc61
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-20 09:30:37 +0700

    Menambahkan nilai Judul Buku dari database pada tabel detail peminjaman

--- pages/peminjaman/peminjamanDetail.php
+++ pages/peminjaman/peminjamanDetail.php
@@ -84,7 +84,7 @@ if ($resultGetDetail) {
                                 <p class="mb-0 text-dark fw-bolder">Judul Buku</p>
                             </td>
                             <td class="col-6 col-md-9">
-                                
+                                <p class="mb-0 text-dark"><?= htmlspecialchars($peminjamanData['judul_buku']); ?></p>
                             </td>
                         </tr>
                     </tbody>
