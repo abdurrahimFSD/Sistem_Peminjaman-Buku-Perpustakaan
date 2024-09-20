e476740cb93862c26a61e652f2b23481e66378f2
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-20 09:21:24 +0700

    Menampilkan nilai Tanggal Pinjam dari database pada tabel detail peminjaman

--- pages/peminjaman/peminjamanDetail.php
+++ pages/peminjaman/peminjamanDetail.php
@@ -68,7 +68,7 @@ if ($resultGetDetail) {
                                 <p class="mb-0 text-dark fw-bolder">Tanggal Pinjam</p>
                             </td>
                             <td class="col-6 col-md-9">
-                                
+                                <p class="mb-0 text-dark"><?= htmlspecialchars($peminjamanData['tanggal_pinjam']); ?></p>
                             </td>
                         </tr>
                     </tbody>
