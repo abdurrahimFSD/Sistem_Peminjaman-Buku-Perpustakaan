82d94ff7295616847955ddd448bfa0ba81798a2f
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-20 10:27:02 +0700

    Menangani response dari fetch sebagai text setelah pengiriman form anggotaCreate

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -111,6 +111,7 @@ if (document.getElementById('peminjamanCreateForm')) {
             method: 'POST',
             body: formData
         })
+        .then(response => response.text());
     })
 
 }
