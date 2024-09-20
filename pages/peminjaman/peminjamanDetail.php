b45ca5174e187129f7e8d17858e0b62091197b77
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-20 09:53:53 +0700

    Menambahkan kolom untuk menampilkan nilai Status

--- pages/peminjaman/peminjamanDetail.php
+++ pages/peminjaman/peminjamanDetail.php
@@ -115,6 +115,9 @@ if ($resultGetDetail) {
                             <td class="col-6 col-md-3">
                                 <p class="mb-0 text-dark fw-bolder">Status</p>
                             </td>
+                            <td class="col-6 col-md-9">
+                                
+                            </td>
                         </tr>
                     </tbody>
                 </table>
