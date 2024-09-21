da821879ace9295d3d551ff0e0571bd0198acf73
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-21 10:08:34 +0700

    Memperbarui kembalian jadi successAnggotaDelete

--- controllers/process.php
+++ controllers/process.php
@@ -72,7 +72,7 @@ if (isset($_GET['id_anggota'])) {
 
     // Jika result mengembalikan true
     if ($result) {
-        echo "successDelete";
+        echo "successAnggotaDelete";
     } else {
         echo "errorDelete";
     }
