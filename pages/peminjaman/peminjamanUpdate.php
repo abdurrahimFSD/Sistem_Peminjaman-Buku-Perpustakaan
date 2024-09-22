8bbe36159175552f333e103b9b77e478a569e719
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-22 10:44:32 +0700

    Menambahkan tombol simpan untuk menyimpan data

--- pages/peminjaman/peminjamanUpdate.php
+++ pages/peminjaman/peminjamanUpdate.php
@@ -109,6 +109,9 @@ if (isset($_GET['id_pinjam'])) {
                     <iconify-icon icon="fluent:arrow-left-24-filled" class="me-1 fs-5"></iconify-icon>Kembali
                 </a>
                 <input type="hidden" name="simpan" value="peminjamanUpdate">
+                <button type="submit" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
+                    <iconify-icon icon="fluent:save-24-regular" class="me-1 fs-5"></iconify-icon>Simpan
+                </button>
             </form>
         </div>
     </div>
