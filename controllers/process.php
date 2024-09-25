43ca34cd19e00f60585561f3381676ffde36afd0
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-25 20:40:23 +0700

    Menghapus kondisi duplikat isbn

--- controllers/process.php
+++ controllers/process.php
@@ -11,11 +11,7 @@ if (isset($_POST['simpan'])) {
         // Jika result mengembalikan success
         if ($result == 'success') {
             header("Location: ../index.php?page=bukuCreate&status=successBukuCreate");
-        } elseif (strpos($result, 'duplicateIsbn') !== false) {
-            // Ambil isbn yg duplicate
-            $isbnDuplicate = explode(':', $result)[1];
-            header("Location: ../index.php?page=bukuCreate&status=duplicateIsbn&isbn=$isbnDuplicate");
-        }
+        } 
     } elseif ($_POST['simpan'] == 'bukuUpdate') {   // Jika tombol simpan adalah bukuUpdate
         // Memanggil function bukuUpdate
         $result = bukuUpdate($_POST, $_FILES);
