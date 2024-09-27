ddd69aa2ededb25b7c10678b680006635ec4a0e6
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-27 21:31:52 +0700

    Menghapus kode isbn

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -1,7 +1,6 @@
 // Mengecek apakah URL mengandung parameter status dan isbn
 const urlParams = new URLSearchParams(window.location.search);
 const status = urlParams.get('status');
-const kodeIsbn = urlParams.get('isbn');
 
 
 if (document.getElementById('peminjamanCreateForm')) {
