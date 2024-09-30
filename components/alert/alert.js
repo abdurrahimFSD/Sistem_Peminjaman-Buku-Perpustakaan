f90d5cd47b196e2de4b9bdc67263f2e127d71acd
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-30 17:02:30 +0700

    Menambahkan confirmButtonText 'Simpan' ke dalam alert

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -208,6 +208,7 @@ if (document.getElementById('anggotaUpdateForm')) {
         Swal.fire({
             title: 'Apakah anda ingin menyimpan perubahan ini',
             showCancelButton: true,
+            confirmButtonText: 'Simpan',
         })
     })
 }
