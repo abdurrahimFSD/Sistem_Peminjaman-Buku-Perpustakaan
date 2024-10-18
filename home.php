606feeda34a6d350c4e6b833151a08327c6a4a13
Abdurrahim <abdurrahim22code@gmail.com>
2024-10-18 10:23:41 +0800

    Menambahkan card untuk menampilkan Total Buku

--- home.php
+++ home.php
@@ -6,7 +6,27 @@ include('./controllers/function.php');
 <div class="body-wrapper ">
     <div class="container-fluid">
         
-        <div class="row"></div>
+        <div class="row">
+            
+            <!-- Card Start Total Buku -->
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row gap-6 align-items-center">
+                            <div class="round-40 rounded-circle text-white d-flex align-items-center justify-content-center text-bg-primary">
+                                <iconify-icon icon="tabler:book-2" class="fs-6"></iconify-icon>
+                            </div>
+                            <div class="align-self-center">
+                                <h4 class="card-title mb-1">20</h4>
+                                <p class="card-subtitle">Total Buku</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Card End Total Buku -->
+            
+        </div>
 
     </div>
 </div>
