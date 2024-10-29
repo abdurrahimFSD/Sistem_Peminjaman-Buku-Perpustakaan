398dddd37279b5e383fb147d12dd387f8aa0b941
Abdurrahim <abdurrahim22code@gmail.com>
2024-10-29 17:35:18 +0800

    Menambahkan keterangan error saat proses signup

--- pages/auth/signup.php
+++ pages/auth/signup.php
@@ -66,14 +66,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                     <div class="mb-3">
                                         <label for="username" class="form-label">Username</label>
                                         <input type="text" class="form-control" id="username" name="username" required aria-describedby="textHelp" placeholder="admin1">
+                                        <div id="usernameError" class="text-danger mt-1 fw-bold"></div>
                                     </div>
                                     <div class="mb-3">
                                         <label for="email" class="form-label">Email address</label>
                                         <input type="email" class="form-control" id="email" name="email" required aria-describedby="emailHelp" placeholder="example@gmail.com">
+                                        <div id="emailError" class="text-danger mt-1 fw-bold"></div>
                                     </div>
                                     <div class="mb-4">
                                         <label for="password" class="form-label">Password</label>
                                         <input type="password" class="form-control" id="password" name="password" required placeholder="**********">
+                                        <div class="text-danger mt-1 fw-bold">
+                                            <?php if (isset($error)) { echo "<p>$error</p>"; } ?>
+                                        </div>
                                     </div>
                                     <button type="submit" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign Up</button>
                                     <div class="d-flex align-items-center">
